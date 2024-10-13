2cf5cda43f8b711771dcad191ef23a4464feb536
Bruno Guratti <123127842+brunoguratti@users.noreply.github.com>
2024-10-13 18:09:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -387,6 +387,6 @@ if ss.stage > 0:
         st.error("Please upload a resume and paste a job description.")
 st.markdown("\n\n")
 logo = "assets/images/bganal_bw.png"
-left_co, cent_co,last_co = st.columns(3)
+left_co, cent_co,last_co = st.columns(5)
 with cent_co:
-    st.image(logo, width=130)
+    st.image(logo)
